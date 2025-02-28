ffeed8f1b06dafe781b33ec2e86b3bcf7c77e42c
SVK-414 <151344783+SVK-314@users.noreply.github.com>
2025-02-28 18:42:03 +0300

    Add files via upload

---
+++ админ/klienti.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.klienti(
+	id_kli, familia, ima, otchestvo, email, phone)
+	VALUES (1, 'Иванов', 'Алексей', 'Сергеевич', 'ivanaa@gmail.com', '+79123456789'),
+	(2, 'Петрова', 'Мария', 'Вальдемаровна', 'maaariya@gmail.com', '+79824671942'),
+	(3, 'Сидоров', 'Дмитрий', 'Артурович', 'kot@gmail.com', '+78349251630'),
+	(4, 'Кузнец', 'Антуан', 'Иванович', 'tryggght@gmail.com', '+754618367374');
---
+++ админ/sotry.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.sotrydniki(
+	id_sotrydnika, familia, ima, otchestvo, datee, email, phone)
+	VALUES 
+	(1, 'Смирнов', 'Олег', 'Альбинович', '19.08.1995', 'smirnov22@gmail.com', '+79123752857'),
+	(2, 'Трудин', 'Альберт', 'Вадимович', '25.10.2000', 'try2@gmail.com', '+79042658361'),
+	(3, 'Хабиб', 'Артур', 'Ахрамович', '02.12.1985', 'artyrchik228@gmail.com', '+79236473827'),
+	(4, 'Габин', 'Макар', 'Дмитриевич', '14.06.1999', 'shhhhit@gmail.com', '+78328741463');
